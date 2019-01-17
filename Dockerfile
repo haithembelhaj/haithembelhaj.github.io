4686b2c09856bbf619a7a53c9eacc37e1f738c27
haithem bel haj <haythem.belhaj@gmail.com>
2019-01-17 18:30:52 +0100

    add linkedin

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:boron
+FROM node:8
 
 EXPOSE 3000
 
--- app/components/links/template.jsx
+++ app/components/links/template.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
 
-export  default (props) => {
-
-  return (
-    <div {...props} className={'links '+ props.className}>
-      <a className="links--link is-email" href="mailto:haythem.belhaj@gmail.com" alt="email"></a>
-      <a className="links--link is-github" href="https://github.com/haithembelhaj" alt="github"></a>
-      <a className="links--link is-twitter" href="https://twitter.com/haythembelhaj" alt="twitter"></a>
-      <a className="links--link is-xing" href="https://www.xing.com/profile/Haithem_belHaj" alt="xing"></a>
-    </div>
-  );
-}
+export default props => {
+	return (
+		<div {...props} className={"links " + props.className}>
+			<a
+				className="links--link is-email"
+				href="mailto:haythem.belhaj@gmail.com"
+				alt="email"
+			/>
+			<a
+				className="links--link is-github"
+				href="https://github.com/haithembelhaj"
+				alt="github"
+			/>
+			<a
+				className="links--link is-twitter"
+				href="https://twitter.com/haythembelhaj"
+				alt="twitter"
+			/>
+			<a
+				className="links--link is-xing"
+				href="https://www.xing.com/profile/Haithem_belHaj"
+				alt="xing"
+			/>
+			<a
+				className="links--link is-linkedin"
+				href="https://www.linkedin.com/in/haithem-bel-haj-292957176/"
+				alt="linkedin"
+			/>
+		</div>
+	);
+};
