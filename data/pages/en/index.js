2289f75a1b57dc00f83b15a89e7ddb9c1c75f8b6
haithem bel haj <haythem.belhaj@gmail.com>
2019-01-17 18:05:03 +0100

    edit content

--- data/pages/en/index.js
+++ data/pages/en/index.js
@@ -1,31 +1,30 @@
-const marked = require('marked');
+const marked = require("marked");
 
 module.exports = {
+	title: "A fullstack develper!",
+	description: "Hi! I'm a frontend engineer with serious backend skills",
+	content: {
+		hero: {
+			first: "Hi!",
+			second: "I'm a frontend engineer with some serious backend skills."
+		},
+		about: marked(`
+I\'m fluent in ReactJS, NodeJS and Python and keen to learn the cool new stuff.
 
-  title: 'A fullstack develper!',
-  description: 'Hi! I\'m a frontend engineer with serious backend skills',
-  content: {
-    hero: {
-      first: 'Hi!',
-      second: 'I\'m a frontend engineer with some serious backend skills.'
-    },
-    about: marked(`
-I\'m fluent in ReactJS, NodeJS and PHP and keen to learn the cool new stuff.
-      
 I'm an expert in web applications but also enjoy creating standalone apis.
-      
+
 I have experience with micro services and isomorphic & native apps.
-      
+
 I like to build my own tools.
-      
-I work for digital agencies like [aperto](http://www.aperto.com) and [taikonauten](https://taikonauten.com).
+
+I'm currently a member of this amazing collective [konekto](http://www.konek.to) and trying to save the world one byte at a time.
     `),
-    contact: marked(`
+		contact: marked(`
 That's me!
-      
-I'm currently living in the amazing city of Berlin but also interested in remote work.
-      
+
+I'm currently living in the amazing city of Berlin.
+
 So check me out!
     `)
-  }
-}
+	}
+};
